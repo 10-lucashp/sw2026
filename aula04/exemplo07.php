178e882d704237d77e72ab66cf896455da986583
10-lucashp <honoratopereiralucas99@gmail.com>
2026-03-11 19:23:05 -0300

    finalizado os exercicios

--- aula04/exemplo07.php
+++ aula04/exemplo07.php
@@ -1,8 +1,12 @@
 <?php
-//Exercicío 2 pág 45 
+// Exercício 2 pág 45
+
   $r = 5;
   $pi = 3.141592;
-  $area = ((4 * $pi) * $r) * $area
 
+  $area = 4 * $pi * ($r * $r);
+  $volume = (4/3) * $pi * ($r * $r * $r);
 
+  echo "area: $area";
+  echo "volume da esfera: $volume";
 ?>
---
+++ exemplo08.php
@@ -0,0 +1,22 @@
+<?php
+// Exercício 3 pág 45
+
+  $a = 10;
+  $b = 3;
+  $adicao = $a + $b;
+  $subtracao = $a - $b;
+  $divisao = $a / $b;
+  $multiplicacao = $a * $b;
+  $restodadivisao = $a % $b;
+
+
+  echo "<br>Valor A: $a <br>";
+  echo "<br>Valor B: $b <br>";
+  echo "<br>adição: $adicao <br>";
+  echo "<br>subtração: $subtracao <br>";
+  echo "<br>multiplicação: $multiplicacao <br>";
+  echo "<br>divisão: $divisao <br>";
+  echo "<br>Resto da divisão:  $restodadivisao <br>";
+ 
+
+?>
