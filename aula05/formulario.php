6e38be94f1d8ec2439295811bf8cb0e9a85c2cd4
Lucas Honorato Pereira <honoratopereiralucas99@gmail.com>
2026-03-12 08:28:39 -0300

    Add files via upload

---
+++ aula05/formulario.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="processa.php"  metode="GET">
+        <p>Nome: <input type="text" name="" id=""></p>
+        <p>Email: <input type="email" name="" id=""></p>
+        <p>Idade: <input type="number" name="" id=""></p>
+        <input type="submit" value="ENVIAR">
+    </form>
+    
+</body>
+</html>
---
+++ aula05/processa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processa Dados</title>
+</head>
+<body>
+    <?php
+        $nome = $_GET['nome'];
+        $email = $_GET['email'];
+        $idade = $_GET ['idade'];
+
+        echo "O nome é: $nome";
+    ?>
+</body>
+</html>
