a159d92fd449baa375ebdfe9105bda7e0ab8943b
Lucas Honorato Pereira <honoratopereiralucas99@gmail.com>
2026-03-26 12:22:00 -0300

    Add files via upload

---
+++ aula06/restrita.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>área restore_include_path</title>
+</head>
+<body>
+    <p>Bem vindo <?php echo $nome; ?> a área restrita!!!</p>  
+</body>
+</html>
