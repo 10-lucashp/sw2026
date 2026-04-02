b93463b484a2b4442fd5114e8356cafb3df7bacb
10-lucashp <honoratopereiralucas99@gmail.com>
2026-04-02 12:21:45 -0300

    gg

---
+++ atividade1bimestre/processa.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    // email: adm@gmail.com
+    // senha: 1234
+    
+    if ($email == 'adm@gmail.com' && $senha == '1234') {
+        // vamos para a área restrita...
+        //echo "vamos para a área restrita...";
+        $nome = "Lucas";
+
+        header('Location: telainicialposlogin.php?nome=' . $nome);
+    } else {
+        header('Location: error.php');
+    }
+
+
+
+    ?>
+</body>
+
+</html>
